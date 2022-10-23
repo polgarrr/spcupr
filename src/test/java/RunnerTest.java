29d5b894bb61f7746bc9c802de0cebf98a1909a5
”Rita <ksmnlu@gmail.com>
2022-10-23 23:34:26 +0300

    Initial commit

---
+++ src/test/java/RunnerTest.java
@@ -0,0 +1,17 @@
+//import cucumber.api.CucumberOptions;
+//import cucumber.api.SnippetType;
+//import cucumber.api.junit.Cucumber;
+//import org.junit.runner.RunWith;
+//
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+//        features = "src/test/features",
+////        glue = пакет, в котором находятся классы с реализацией шагов и «хуков»,
+//        tags = "@all",
+//        dryRun = false,
+//        strict = false,
+//        snippets = SnippetType.UNDERSCORE
+////        name = "^Успешное|Успешная.*" фильтрует запускаемые тесты по названиям удовлетворяющим регулярному выражению
+//)
+//public class RunnerTest {
+//}
---
+++ src/test/java/com/example/spcupr/SpcuprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spcupr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpcuprApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
